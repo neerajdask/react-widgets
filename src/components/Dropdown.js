638af11571e4cfd902e7823072350d27e5091ade
neerajdask <iamneerajdas@gmail.com>
2021-01-11 11:24:23 +0530

    Added Convert and TRanslate Widget

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,29 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange, label }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    }, []);
+
     const renderdOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
         return (
-            <div className='item' key={option.value}>
+            <div
+                className='item'
+                key={option.value}
+                onClick={() => {
+                    onSelectedChange(option);
+                }}>
                 {option.label}
             </div>
         );
     });
     return (
-        // <div>
-        <div className='ui form'>
+        <div ref={ref} className='ui form'>
             <div className='field'>
-                <label className='label'>Select a color</label>
-                <div className='ui selection dropdown visible active'>
+                <label className='label'>{label}</label>
+                <div
+                    onClick={() => {
+                        setOpen(!open);
+                    }}
+                    className={`ui selection dropdown ${
+                        open ? 'visible active' : ''
+                    }`}>
                     <i className='dropdown icon'></i>
-                    <div className='text'>Select color</div>
-                    <div className='menu visible transition'>
+                    <div className='text'>{selected.label}</div>
+                    <div
+                        className={`menu ${
+                            open ? 'visible transition' : ''
+                        }`}>
                         {renderdOptions}
                     </div>
                 </div>
             </div>
         </div>
-        // </div>
     );
 };
 export default Dropdown;
